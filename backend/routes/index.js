0b86cdedd07c0d52fb8c405b4a32df3216ad7625
Marl-Sant <santosfmarlon@gmail.com>
2025-03-28 17:53:16 -0400

    Added beginnings of NavBar

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,15 +1,5 @@
-// backend/routes/index.js
 const express = require('express');
 const router = express.Router();
-
-router.get('/api/csrf/restore', (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie('XSRF-TOKEN', csrfToken);
-  res.status(200).json({
-    'XSRF-Token': csrfToken,
-  });
-});
-
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
@@ -21,38 +11,31 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at the root route
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
-      path.resolve(
-        __dirname,
-        '../../frontend',
-        'dist',
-        'index.html'
-      )
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
     );
   });
 
   // Serve the static assets in the frontend's build folder
-  router.use(express.static(path.resolve('../frontend/dist')));
+  router.use(express.static(path.resolve("../frontend/dist")));
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
-      path.resolve(
-        __dirname,
-        '../../frontend',
-        'dist',
-        'index.html'
-      )
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
     );
   });
 }
 
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
-  router.get('/api/csrf/restore', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
+  router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+      'XSRF-Token': csrfToken
+    });
   });
 }
 
