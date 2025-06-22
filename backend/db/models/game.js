ca740a352f1609f8e82f027da04b0430bbfa2592
Dartanhan Ramos <alessandroramosd@gmail.com>
2025-06-22 16:02:39 -0400

    added foreign keys to user and game tables

--- backend/db/models/game.js
+++ backend/db/models/game.js
@@ -14,6 +14,10 @@ module.exports = (sequelize, DataTypes) => {
       Game.hasMany(models.Review, {
         foreignKey: "gameId"
       });
+
+      Game.hasMany(models.GameImage, {
+        foreignKey: "gameId"
+      })
     }
   }
   Game.init({
