26d68e1e430adc36352a4d4b9f35bf0f421ba703
Dartanhan Ramos <alessandroramosd@gmail.com>
2025-06-27 21:26:41 -0400

    Added a carousel of mini pictures with working scroll area

--- frontend/src/components/GameDetailPage/GameDetailPage.jsx
+++ frontend/src/components/GameDetailPage/GameDetailPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import * as gamesAction from '../../store/games';
 import * as reviewsAction from '../../store/reviews';
 import { useDispatch, useSelector } from 'react-redux';
@@ -9,7 +9,8 @@ import ReviewArea from '../ReviewArea/ReviewArea'
 function GameDetailPage() {
   const { gameId } = useParams();
   const dispatch = useDispatch();
-  
+  const thumbnailContainerRef = useRef(null);
+
   useEffect(() => {
     dispatch(gamesAction.getGameById(gameId));
     dispatch(reviewsAction.setReviewsState(gameId));
@@ -20,43 +21,60 @@ function GameDetailPage() {
   const game = useSelector((state) => state.games?.currentGame);
   const reviews = useSelector((state) => state.reviews?.allReviews);
 
+  const scrollThumbnails = (direction) => {
+    const container = thumbnailContainerRef.current;
+    const scrollAmount = 300;
+    if (container) {
+      container.scrollLeft += direction === 'right' ? scrollAmount : -scrollAmount;
+    }
+  };
+
   return (
     <div className='game-page-background'>
       <div className='game-container'>
-          
-          <div className='game-title'>
-            {game?.title}
-          </div>
-          
-          <div className='game-image'>
-            <div className='primary-game-container'>
-              <img 
-                src={game?.GameImages.find(img => img.displayPic === false).url}
-                alt={`${game?.title} display`}
-                className='primary-game-image'
-              />
-              <div className='mini-image-container'>
-                {game?.GameImages?.map(img => (
+        
+        <div className='game-title'>
+          {game?.title}
+        </div>
+        
+        <div className='game-image'>
+          <div className='primary-game-container'>
+            <img 
+              src={game?.GameImages.find(img => img.displayPic === false)?.url}
+              alt={`${game?.title} display`}
+              className='primary-game-image'
+            />
+
+            <div className='thumbnail-carousel-wrapper'>
+              <div className='mini-image-container' ref={thumbnailContainerRef}>
+                {game?.GameImages?.map((img, index) => (
                   <img 
                     src={img.url} 
+                    alt={`Thumbnail ${index}`}
                     className='mini-images' 
-                    key={img}
+                    key={index}
                   />
                 ))}
               </div>
-          </div>
 
-            <div className='secondary-image-container'>
-              <img 
-                src={game?.GameImages?.find(img => img.displayPic === true)?.url}
-                alt={`${game?.title} secondary`}
-                className='secondary-game-image'
-              />
-              <div className='game-description'>
-                {game?.description}
+              <div className="carousel-scroller">
+                <button className='side-scroll-arrow left' onClick={() => scrollThumbnails('left')}>&lt;</button>
+                <button className='side-scroll-arrow right' onClick={() => scrollThumbnails('right')}>&gt;</button>
               </div>
             </div>
-          </div>     
+          </div>
+
+          <div className='secondary-image-container'>
+            <img 
+              src={game?.GameImages?.find(img => img.displayPic === true)?.url}
+              alt={`${game?.title} secondary`}
+              className='secondary-game-image'
+            />
+            <div className='game-description'>
+              {game?.description}
+            </div>
+          </div>
+        </div>     
       </div>
 
       <div className='review-container'>
