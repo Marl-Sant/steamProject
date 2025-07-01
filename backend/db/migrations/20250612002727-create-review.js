8475e3e1b397d85509e5f57df4c67f3cb72c21c3
Alexander Barrera <barreraoew@gmail.com>
2025-07-01 14:15:34 -0400

    fixed syntax

--- backend/db/migrations/20250612002727-create-review.js
+++ backend/db/migrations/20250612002727-create-review.js
@@ -8,38 +8,41 @@ if (process.env.NODE_ENV === "production") {
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Reviews', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      review: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      userId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      gameId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
+    await queryInterface.createTable(
+      "Reviews",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        review: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        userId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        gameId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
         isRecommended: {
           type: Sequelize.BOOLEAN,
-          allowNull: false,    
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+          allowNull: false,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
       },
       options
     );
