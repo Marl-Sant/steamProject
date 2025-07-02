1a6899639e01d827c672fa88f4071554587fb348
Marl-Sant <santosfmarlon@gmail.com>
2025-07-02 17:07:24 -0400

    trying a dynamic approach

--- backend/db/seeders/20250622204405-review-likes.js
+++ backend/db/seeders/20250622204405-review-likes.js
@@ -1,46 +1,64 @@
 'use strict';
-const { ReviewLike } = require('../models')
+const { ReviewLike } = require('../models');
+const { Review } = require('../models');
 
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
 }
 
+const allReviews = await Review.findAll();
+
+const seededReviewLikes = allReviews.map((review, i) => {
+  let reviewLike = {};
+  reviewLike[ownerId] = i + 1;
+  reviewLike[reviewId] = review.id;
+  reviewLike[liked] = i % 2;
+  return reviewLike;
+});
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await ReviewLike.bulkCreate([
-      {
-        ownerId: 1,
-        reviewId: 1,
-        liked: true
-      },
-      {
-        ownerId: 2,
-        reviewId: 2,
-        liked: false
-      },
-      {
-        ownerId: 3,
-        reviewId: 3,
-        liked: true
-      },
-    ])
+  async up(queryInterface, Sequelize) {
+    await ReviewLike.bulkCreate(
+      seededReviewLikes
+      //   [
+      //   {
+      //     ownerId: 1,
+      //     reviewId: 1,
+      //     liked: true
+      //   },
+      //   {
+      //     ownerId: 2,
+      //     reviewId: 2,
+      //     liked: false
+      //   },
+      //   {
+      //     ownerId: 3,
+      //     reviewId: 3,
+      //     liked: true
+      //   },
+      // ]
+    );
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-    options.tableName = "ReviewLikes";
+    options.tableName = 'ReviewLikes';
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options, {
-      ownerId: {
-        [Op.in] : [1, 2, 3]
-      }
-    }, {})
-  }
+    return queryInterface.bulkDelete(
+      options,
+      {
+        ownerId: {
+          [Op.in]: [1, 2, 3],
+        },
+      },
+      {}
+    );
+  },
 };
