d4dbb7690fb497c766504d9940f1c2bacb741ecb
Marl-Sant <santosfmarlon@gmail.com>
2025-07-01 21:27:35 -0400

    added steam api seeding

--- backend/db/migrations/20250325134622-create-game.js
+++ backend/db/migrations/20250325134622-create-game.js
@@ -8,51 +8,102 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Games', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
+    await queryInterface.createTable(
+      'Games',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        title: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        price: {
+          allowNull: false,
+          type: Sequelize.FLOAT,
+        },
+        shortDescription: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        detailedDescription: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        headerImage: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        capsuleImage: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        website: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        requirements: {
+          allowNull: false,
+          type: Sequelize.JSONB,
+        },
+        legal: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        genres: {
+          allowNull: false,
+          type: Sequelize.JSONB,
+        },
+        developers: {
+          allowNull: false,
+          type: Sequelize.JSONB,
+        },
+        publishers: {
+          allowNull: false,
+          type: Sequelize.JSONB,
+        },
+        screenshots: {
+          allowNull: false,
+          type: Sequelize.JSONB,
+        },
+        movies: {
+          allowNull: false,
+          type: Sequelize.JSONB,
+        },
+        releaseDate: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        background: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        ESRBRating: {
+          type: Sequelize.JSONB,
+        },
+        categories: {
+          allowNull: false,
+          type: Sequelize.JSONB,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        },
       },
-      title: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      price: {
-        type: Sequelize.FLOAT,
-        allowNull: false
-      },
-      description: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      genre: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      developer: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      publisher: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-      }
-    }, options);
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
-    options.tableName = "Games";
+    options.tableName = 'Games';
     return queryInterface.dropTable(options);
-  }
+  },
 };
--- backend/db/seeders/20250328184220-demo-games.js
+++ backend/db/seeders/20250328184220-demo-games.js
@@ -10,35 +10,99 @@ if (process.env.NODE_ENV === 'production') {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await Game.bulkCreate([
-      {
-        title: 'Final Fantasy XI',
-        price: 59.99,
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-        genre: 'Tactics',
-        developer: 'Square Enix',
-        publisher: 'Sony',
-      },
-      {
-        title: 'Halo',
-        price: 19.99,
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-        genre: 'First Person Shooter',
-        developer: 'Bungie',
-        publisher: 'Microsoft',
-      },
-      {
-        title: 'The Sims',
-        price: 49.99,
-        description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-        genre: 'Simulator',
-        developer: 'EA',
-        publisher: 'EA',
-      },
-    ]);
+    const appIdArray = [
+      1151340, 1203620, 1627720, 2246340, 594330, 1361210,
+      1086940, 1313140, 1222140, 601430, 1670870, 976310,
+    ];
+
+    const seedGameInformation = await Promise.all(
+      appIdArray.map(async (id) => {
+        const response = await fetch(
+          `https://store.steampowered.com/api/appdetails?appids=${id}`
+        );
+        const jsonResponse = await response.json();
+        const base = jsonResponse[`${id}`]['data'];
+        let seedGame = {};
+        seedGame['title'] = base['name'];
+        seedGame['shortDescription'] = base['short_description'];
+        seedGame['detailedDescription'] =
+          base['detailed_description'];
+        seedGame['headerImage'] = base['header_image'];
+        seedGame['capsuleImage'] = base['capsule_image'];
+        seedGame['website'] = base['website'];
+        seedGame['requirements'] = base['pc_requirements'];
+        seedGame['legal'] = base['legal_notice'];
+        seedGame['publishers'] = base['publishers'];
+        seedGame['developers'] = base['developers'];
+        seedGame['price'] = '59.99';
+        seedGame['categories'] = base['categories'].map(
+          (category) => {
+            return category['description'];
+          }
+        );
+        seedGame['genres'] = base['genres'].map((genre) => {
+          return genre['description'];
+        });
+        seedGame['screenshots'] = base['screenshots'].map(
+          (image) => {
+            return image['path_thumbnail'];
+          }
+        );
+        seedGame['movies'] = base['movies'].map((movie) => {
+          return movie['mp4']['480'];
+        });
+        seedGame['releaseDate'] = base['release_date']['date'];
+        seedGame['background'] = base['background'];
+        seedGame['ESRBRating'] = base['ratings']['esrb'];
+
+        console.log(seedGame.price, seedGame.title);
+        return seedGame;
+      })
+    );
+
+    // console.log(seedGameInformation);
+    // const finalFantasy = await fetch(
+    //   'https://store.steampowered.com/api/appdetails?appids=230330'
+    // );
+
+    // const finalFantasyData = await finalFantasy.json();
+    // console.log(
+    //   finalFantasyData[230330]['data'],
+    //   'THIS IS BEING HIT'
+    // );
+
+    await Game.bulkCreate(
+      seedGameInformation
+      //   [
+      //   {
+      //     title: finalFantasy[230330]['data']['name'],
+      //     price: 59.99,
+      //     description:
+      //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      //     genre: 'Tactics',
+      //     developer: 'Square Enix',
+      //     publisher: 'Sony',
+      //   },
+      //   {
+      //     title: 'Halo',
+      //     price: 19.99,
+      //     description:
+      //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      //     genre: 'First Person Shooter',
+      //     developer: 'Bungie',
+      //     publisher: 'Microsoft',
+      //   },
+      //   {
+      //     title: 'The Sims',
+      //     price: 49.99,
+      //     description:
+      //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+      //     genre: 'Simulator',
+      //     developer: 'EA',
+      //     publisher: 'EA',
+      //   },
+      // ]
+    );
   },
 
   async down(queryInterface, Sequelize) {
