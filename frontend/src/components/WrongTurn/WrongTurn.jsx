8b7b5c6bad64a4edefe311f88f046de97488c543
Marl-Sant <santosfmarlon@gmail.com>
2025-07-28 21:11:07 -0400

    added an error landing page

--- frontend/src/components/WrongTurn/WrongTurn.jsx
+++ frontend/src/components/WrongTurn/WrongTurn.jsx
@@ -1,6 +1,8 @@
 import './WrongTurn.css';
 import { IoConstructSharp } from "react-icons/io5";
 import { LuConstruction } from "react-icons/lu";
+import { MdOutlineUTurnLeft } from "react-icons/md";
+import { MdOutlineUTurnRight } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 
 function WrongTurn() {
@@ -13,7 +15,7 @@ function WrongTurn() {
     <div className=''>
         Whoopsie! Looks like you stumbled onto a page we are still working on or asked for a page that doesn&apos;t exist. No worries! We are working hard to make sure you have a great time on Gleam and to have it work as anticipated. Mind the dust while we get you back to the home page!
     </div>
-    <NavLink to='/'>Home page</NavLink>
+    <p className='way-home'><MdOutlineUTurnLeft /><NavLink to='/'>Home page</NavLink><MdOutlineUTurnRight /></p>
     </div>
     </>
   );
