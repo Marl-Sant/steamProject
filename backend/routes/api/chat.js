6e083f1b2e775769e137314f85ee45c8a2584a23
Marl-Sant <santosfmarlon@gmail.com>
2025-08-10 22:05:02 -0400

    ai_feature part 2

--- backend/routes/api/chat.js
+++ backend/routes/api/chat.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const openai = require('../../lib/openai');
 const { Game } = require('../../db/models');
+const { User } = require('../../db/models');
+const { Ownership } = require('../../db/models');
 const { Op } = require('sequelize');
 
 const tools = [
@@ -10,7 +12,7 @@ const tools = [
     function: {
       name: 'getGameFromDB',
       description:
-        'Looks up information about a game in our database or Steam website if not found in our database based on the title',
+        'Looks up information about a game in our database or Steam website if not found in our database. Use this function if the user specifically asks for the detail of a game and provides the title. These recommendations should always include a link. If the game is in our database, create a link and use the following URL http://localhost:5173/games/{game.id} where game.id is provided value from a return.',
       parameters: {
         type: 'object',
         properties: {
@@ -28,7 +30,7 @@ const tools = [
     function: {
       name: 'recommendGame',
       description:
-        'Recommends a game from our database based on the user input.',
+        'Recommends a game by searching through our database to find matching keywords in our Games detailedDescription. Will also recommend a game by searching Steam for the game if it is not in our database. These recommendations should always include a link. If the game is in our database, create a link and use the following URL http://localhost:5173/games/{game.id} where game.id is provided value from a return.  Always provide at least 3 recommendation and notify the user if the link leads outside of our website. Use this function if the user does not provide you a title and does not reference any games that they own.',
       parameters: {
         type: 'object',
         properties: {
@@ -50,47 +52,79 @@ const tools = [
 router.post('/', async (req, res) => {
   const { message } = req.body;
 
+  const baseRequest = {
+    model: 'gpt-5-mini',
+    messages: [
+      {
+        role: 'system',
+        content:
+          'You are a helpful assistant at a video game store. Users will often ask you to recommend games in a variety of ways. You only know information regarding video games and will not answer questions outside that topic. When a user asks you about a video game, you will always check the database for information to provide them. If the video game is not in the database, you will provide a link to Steam with that game as a search query. You will provide as short description of the game and the features, the developer, and the price.',
+      },
+      { role: 'user', content: message },
+    ],
+    tools,
+  };
+
   try {
     const initialResponse = await openai.chat.completions.create(
       {
+        ...baseRequest,
         model: 'gpt-5-mini',
-        messages: [
-          {
-            role: 'system',
-            content:
-              'You are a very helpful assistant at a video game store. You know nothing outside of video games and will not provide any information about other topics. When a user asks a question only give information about video games. Once the question is asked check our database to see if the video game has an entry. If it does, provide a explanation of the video game that includes a shortened version of the detailedDescription, the developer, and the price.',
-          },
-          { role: 'user', content: message },
-        ],
         tools,
-        tool_choice: 'auto',
       }
     );
 
-    console.log(initialResponse.choices[0].message.tool_calls);
+    console.log(
+      initialResponse.choices[0].message.tool_calls?.[0]
+    );
+
     const toolCall =
       initialResponse.choices[0].message.tool_calls?.[0];
 
-    if (toolCall['function']['name'] === 'getGameFromDB') {
-      const args = JSON.parse(toolCall.function.arguments);
-      const { title } = args;
+    let game;
+    let toolResponse;
 
-      const game = await Game.findOne({
-        where: { title },
-      });
+    if (toolCall) {
+      if (toolCall['function']['name'] === 'getGameFromDB') {
+        const args = JSON.parse(toolCall.function.arguments);
+        const { title } = args;
 
-      const toolResponse = game
-        ? {
-            id: game.id,
-            title: game.title,
-            descriptions: {
-              shortDescriptions: game.shortDescription,
-              detailedDescriptions: game.detailedDescription,
-            },
-            genre: game.genre,
-            price: game.price,
-          }
-        : null;
+        game = await Game.findOne({
+          where: { title },
+        });
+
+        toolResponse = game
+          ? {
+              id: game.id,
+              title: game.title,
+              descriptions: {
+                shortDescriptions: game.shortDescription,
+                detailedDescriptions: game.detailedDescription,
+              },
+              genre: game.genre,
+              price: game.price,
+            }
+          : null;
+      } else if (
+        toolCall['function']['name'] === 'recommendGame'
+      ) {
+        const args = JSON.parse(toolCall.function.arguments);
+        const { keywords } = args;
+
+        let searchParams = {
+          where: {},
+        };
+        if (keywords)
+          searchParams.where.detailedDescription = {
+            [Op.or]: keywords.map((keyword) => ({
+              [Op.substring]: `${keyword}`,
+            })),
+          };
+
+        game = await Game.findAll(searchParams);
+
+        toolResponse = game ? game.join(' ') : null;
+      }
 
       const followUp = await openai.chat.completions.create({
         model: 'gpt-5-mini',
@@ -105,7 +139,7 @@ router.post('/', async (req, res) => {
           {
             role: 'tool',
             tool_call_id: toolCall.id,
-            name: 'getGameFromDB',
+            name: toolCall['function']['name'],
             content: JSON.stringify(toolResponse),
           },
         ],
@@ -113,51 +147,6 @@ router.post('/', async (req, res) => {
 
       return res.json({
         reply: followUp.choices[0].message.content,
-      });
-    } else if (
-      toolCall['function']['name'] === 'recommendGame'
-    ) {
-      const args = JSON.parse(toolCall.function.arguments);
-      const { keywords } = args;
-      let searchParams = {
-        where: {},
-      };
-      if (keywords)
-        searchParams.where.detailedDescription = {
-          [Op.or]: keywords.map((keyword) => ({
-            [Op.substring]: `${keyword}`,
-          })),
-        };
-
-      const game = await Game.findAll(searchParams);
-
-      const toolResponse = game ? game : null;
-
-      const followUp = await openai.chat.completions.create({
-        model: 'gpt-5-mini',
-        messages: [
-          {
-            role: 'system',
-            content:
-              'You are a very helpful assistant at a video game store. You know nothing outside of video games and will not provide any information about other topics. When a user asks a question only give information about video games. Once the question is asked check our database to see if the video game has an entry. If it does, provide a explanation of the video game that includes a shortened version of the detailedDescription, the developer, and the price. You should also create a html anchor tag that follows the structure of "http://localhost:5173/games/{game.id} with the game id being supplied by a return from the database. This link should be real HTML and would allow the user to click it and navigate to the game detail page. If the game does not exist in our database, provide the user a link to the Steam search page using the title of the game as a search query. Any links that are directed to Steam should have a header notifying the user they will be leaving our site as well as a professional explanation that we do not have that a game based on the user input in our database. Regardless if a game is or is not in our database, provide the user with at least three recommendations.  Your reply should come back housed in a div. That div should have in-line styling that is simple and consistent with previous messages.',
-          },
-          { role: 'user', content: message },
-          { role: 'assistant', tool_calls: [toolCall] },
-          {
-            role: 'tool',
-            tool_call_id: toolCall.id,
-            name: 'recommendGame',
-            content: JSON.stringify(toolResponse),
-          },
-        ],
-      });
-
-      console.log(followUp.choices, 'LOOK HERE');
-
-      return res.json({
-        reply: followUp.choices
-          .map((game) => game.message.content)
-          .join(' '),
       });
     } else {
       return res.json({
