8b36a0a6dbcda164723adf12567abe4dec4afbf5
Alexander Barrera <barreraoew@gmail.com>
2025-08-20 16:34:53 -0400

    finishing up

--- backend/db/migrations/20250820193915-create-post-comment.js
+++ backend/db/migrations/20250820193915-create-post-comment.js
@@ -8,45 +8,49 @@ if (process.env.NODE_ENV === "production") {
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("PostComments", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      userId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: "Users",
-          key: "id",
+    await queryInterface.createTable(
+      "PostComments",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
         },
-        onDelete: "CASCADE",
-      },
-      postId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: "Posts",
-          key: "id",
+        userId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: "Users",
+            key: "id",
+          },
+          onDelete: "CASCADE",
         },
-        onDelete: "CASCADE",
+        postId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: "Posts",
+            key: "id",
+          },
+          onDelete: "CASCADE",
+        },
+        comment: {
+          type: Sequelize.STRING,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
       },
-      comment: {
-        type: Sequelize.STRING,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-      },
-    });
+      options
+    );
   },
 
   async down(queryInterface, Sequelize) {
